b74e3e4f96b1fc752e573a60b61c6da9e35e53a4
Hasan Kemal Demirci <hasandemirci@teknasyon.com>
2021-11-26 17:53:02 +0300

    Creates some sorting algorithms

---
+++ playground/sort/src/index.ts
@@ -0,0 +1,11 @@
+// import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 50]);
+numbersCollection.sort();
+console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
